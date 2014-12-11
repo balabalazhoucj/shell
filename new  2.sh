604e16d83a323c1ef46f33fd59789e9671dd9fa7
zhoucj <zhoucj98@gmail.com>
2014-12-11 13:11:02 +0800

    tt

---
+++ new  2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#参考  find `pwd` -name 'abc' -type d | xargs rename abc def
+#目录重命名，如果新目录存在则拷贝源目录内文件至现有目录中
+
+D_S=睿泰科技
+D_N=retechwing_rnu
+for f in $(find $(pwd) -name ${D_S} -type d)
+do 
+	
+	cd $(dirname $f) && ls ${D_N} > /dev/null 2>&1
+	if [ $? -ne 0 ];then
+		mv ${D_S} ${D_N}
+	else
+		cp ${D_S}/* ${D_N}/ && rm -rf 
+	fi
+done
+
+
+
+
+
+
+RM{
+mysql_cmd='mysql -uroot -h -e'
+Filepath_cmd=$(${msyql_cmd} 'select FilePath from File where Filename =${dataname} ')
+find $(pwd) -name '$(basename $FilePath_cmd)*' | xargs rm -rf
+}
+
+for i in RM.txt
+do
+	dataname=$i
+	RM
+done
