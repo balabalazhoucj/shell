6da7d1070dc54c9db5f68e7a01f8f41b839816f4
zhoucj <zhoucj98@gmail.com>
2014-12-11 22:40:44 +0800

    优化打印输出

--- login_show_state.sh
+++ login_show_state.sh
@@ -1,9 +1,9 @@
-printf "\tSystem information as of $(date)\n"
-printf "\n"
-echo -e "\tSystem load: $(uptime | awk -F '[ ,]' '{print $14}')         Processes: $(ps -ef | wc -l)\r"
-usage=$(df -h / | awk 'NR==2{print $5 " of " $2 "B"}')
-echo -e "\tUsage of /: ${usage}\t User logged in: $(who | wc -l)\r"
-
+#!/bin/bash
+printf "\n\t System information as of $(date)\n \n"
+systemload=$(uptime | awk -F '[ ,]' '{print $14}')
+processes=$(ps -ef | wc -l)
+usage=$(df -h / | awk 'NR==2{print $5 " " $2 }')
+userlogged=$(who | wc -l)
 Buffers=`grep -we 'Buffers' /proc/meminfo | cut -d' ' -f2- | tr -d "[A-Z][a-z] "`
 Cached=`grep -we 'Cached' /proc/meminfo | cut -d' ' -f2- | tr -d "[A-Z][a-z] "`
 MemFree=`grep -ie 'MemFree' /proc/meminfo | cut -d' ' -f2- | tr -d "[A-Z][a-z] "`
@@ -11,14 +11,16 @@ MemTotal=`grep -ie 'MemTotal' /proc/meminfo | cut -d' ' -f2- | tr -d "[A-Z][a-z]
 SwapCached=`grep -ie 'SwapCached' /proc/meminfo | cut -d' ' -f2- | tr -d "[A-Z][a-z] "`
 SwapFree=`grep -ie 'SwapFree' /proc/meminfo | cut -d' ' -f2- | tr -d "[A-Z][a-z] "`
 SwapTotal=`grep -ie 'SwapTotal' /proc/meminfo | cut -d' ' -f2- | tr -d "[A-Z][a-z] "`
-
 MEMUSED="$(( (  $MemTotal - $MemFree  - $Cached  - $Buffers ) *100 / $MemTotal ))"
-SWAPUSED="$((( $SwapTotal - $SwapFree ) * 100 / $SwapTotal ))"
-echo -e "\tMemory usaage:$MEMUSED%\r"
-echo -e "\tSwap usage:$SWAPUSED%\r"
+SWAPUSED="$((( $SwapTotal - $SwapFree - $SwapCached ) * 100 / $SwapTotal ))"
+
+printf '\t System load: %s \t \t Processes: %s\n' $systemload $processes
+printf '\t Usage of /: %s of %sB \t User logged in: %s \n' $usage $userlogged
+printf '\t Memory usaage: %s%% \t \t Swap usage: %s%% \n' $MEMUSED $SWAPUSED
 
 for i in `ifconfig  | awk '$1~/eth[0-9]/{print $1}'`
 do
 IP=$(ifconfig $i| awk -F'[ :]+' 'NR==2{print $4}')
-echo -e "\tIP address for $i:" $IP
+printf "\t IP address for %s: %s\n" $i $IP
 done
+printf '\n'
