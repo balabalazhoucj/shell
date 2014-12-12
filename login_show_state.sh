c8be330be5d66f5cc2a4aa4d78658d4e917de303
zhoucj <zhoucj98@gmail.com>
2014-12-12 10:13:26 +0800

    增加public ip

--- login_show_state.sh
+++ login_show_state.sh
@@ -12,13 +12,13 @@ SwapFree=`grep -ie 'SwapFree' /proc/meminfo | cut -d' ' -f2- | tr -d "[A-Z][a-z]
 SwapTotal=`grep -ie 'SwapTotal' /proc/meminfo | cut -d' ' -f2- | tr -d "[A-Z][a-z] "`
 MEMUSED="$(( (  $MemTotal - $MemFree  - $Cached  - $Buffers ) *100 / $MemTotal ))"
 SWAPUSED="$((( $SwapTotal - $SwapFree - $SwapCached ) * 100 / $SwapTotal ))"
-
+PUBLICIP=$(curl --connect-timeout 5 -s icanhazip.com)
 
 printf '\n\t System information as of $(date)\n \n'
 printf '\t System load: %s \t \t Processes: %s\n' $systemload $processes
 printf '\t Usage of /: %s of %sB \t User logged in: %s \n' $usage $userlogged
 printf '\t Memory usaage: %s%% \t \t Swap usage: %s%% \n' $MEMUSED $SWAPUSED
-
+printf '\t IP address for Public: %s \n' $PUBLICIP
 for i in `ifconfig  | awk '$1~/eth[0-9]/{print $1}'`
 do
 IP=$(ifconfig $i| awk -F'[ :]+' 'NR==2{print $4}')
