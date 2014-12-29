72fb260f581ecbc6679de57d1c7580d9eb3df6a9
zhoucj <zhoucj98@gmail.com>
2014-12-29 16:33:41 +0800

    增加错误输入判断，优化过程

--- config_check_ip.sh
+++ config_check_ip.sh
@@ -29,4 +29,47 @@ sed -i "s/\(GATE_WAY=\).*/\1${GATE_WAY}/" /etc/sysconfig/network-scripts/ifcfg-e
 
 read -p "input DNS:" DNS
 checkip $DNS
+echo "nameserver $DNS" > /etc/resolv.confcheckip ()
+{
+	ip=$1
+	if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]];then
+		ip=(${ip//./ })
+		for i in ${ip[@]}
+		do
+			if [[ $i -le 255 ]];then
+				single=0 
+			else
+				single=1
+				echo "u input error,pls input again!"
+				break
+			fi
+		done 
+	else
+		single=1
+		echo "u input error,pls input again!"
+	fi
+}
+
+input()
+{
+while [[ $single != 0 ]]
+do
+	read -p "input $1:" value
+	checkip $value
+done
+sed -i "s/\($2=\).*/\1${value}/" /etc/sysconfig/network-scripts/ifcfg-eth0
+}
+single=1
+input "ip address" "IPADDR"
+single=1
+input "NETMASK" "NETMASK"
+single=1
+input "GATEWAY" "GATEWAY"
+
+read -p "input DNS[221.228.255.1]:" DNS
+if [ -z $DNS ];then
+	DNS="221.228.255.1"
+else	
+	checkip $DNS
+fi
 echo "nameserver $DNS" > /etc/resolv.conf
