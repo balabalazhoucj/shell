34dc201bedc546ee984f4b4c5ead6608261f11c4
zhoucj <zhoucj98@gmail.com>
2014-12-29 16:37:13 +0800

    增加错误输入判断，删除重复

--- config_check_ip.sh
+++ config_check_ip.sh
@@ -8,34 +8,6 @@ checkip ()
 		ip=(${ip//./ })
 		for i in ${ip[@]}
 		do
-			[[ $i -le 255 ]] || exit 2
-		done 
-	else
-		exit 3
-	fi
-}
-
-read -p "input ip address:" IP_ADDR
-checkip $IP_ADDR
-sed -i "s/\(IPADDR=\).*/\1${IP_ADDR}/" /etc/sysconfig/network-scripts/ifcfg-eth0
-
-read -p "input netmask:" NET_MASK
-checkip $NET_MASK
-sed -i "s/\(NETMASK=\).*/\1${NET_MASK}/" /etc/sysconfig/network-scripts/ifcfg-eth0
-
-read -p "input GATEWAY:" GATE_WAY
-checkip $GATE_WAY
-sed -i "s/\(GATE_WAY=\).*/\1${GATE_WAY}/" /etc/sysconfig/network-scripts/ifcfg-eth0
-
-read -p "input DNS:" DNS
-checkip $DNS
-echo "nameserver $DNS" > /etc/resolv.confcheckip ()
-{
-	ip=$1
-	if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]];then
-		ip=(${ip//./ })
-		for i in ${ip[@]}
-		do
 			if [[ $i -le 255 ]];then
 				single=0 
 			else
