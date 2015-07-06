ca726386cd9a3f93cdd0bbacf1fc1ad004f0c335
Zhou Chenjie <zhoucj98@gmail.com>
2015-07-06 14:52:51 +0800

    update nginx version

--- nginx.sh
+++ nginx.sh
@@ -1,25 +1,25 @@
 #安装nginx
 yum install pcre-devel openssl-devel -y
 useradd -s /sbin/nologin -M nginx
-mkdir -p /opt/nginx-1.4.5/{run,log}
-ln -s /opt/nginx-1.4.5 /opt/nginx
-wget http://nginx.org/download/nginx-1.4.5.tar.gz
-tar zxf nginx-1.4.5.tar.gz
-cd nginx-1.4.5
-./configure --prefix=/opt/nginx-1.4.5/ \
+mkdir -p /opt/nginx-1.8.0/{run,log}
+ln -s /opt/nginx-1.8.0 /opt/nginx
+wget http://nginx.org/download/nginx-1.8.0.tar.gz
+tar zxf nginx-1.8.0.tar.gz
+cd nginx-1.8.0
+./configure --prefix=/data/nginx-1.8.0/ \
 --user=nginx \
 --group=nginx \
 --with-http_ssl_module \
 --with-http_sub_module \
 --with-http_stub_status_module \
---error-log-path=/opt/nginx/log/error.log   \
---http-log-path=/opt/nginx/log/access.log   \
---pid-path=/opt/nginx/run/nginx.pid \
+--error-log-path=/data/nginx/log/error.log   \
+--http-log-path=/data/nginx/log/access.log   \
+--pid-path=/data/nginx/run/nginx.pid \
 --with-poll_module \
 --with-http_realip_module\
 --with-http_flv_module \
 --with-http_mp4_module
 make && make install
-ln -s /opt/nginx-1.4.5 /opt/nginx
+ln -s /opt/nginx-1.8.0 /opt/nginx
 cd /opt/nginx/conf
 egrep -v "^$|#" nginx.conf.default >nginx.conf
